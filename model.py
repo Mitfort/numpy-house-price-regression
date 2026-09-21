fac62f8a0c8babaf525539b4668482c55ebad979
Mateusz Poszelężny <110680320+Mitfort@users.noreply.github.com>
2026-09-21 14:06:40 +0200

    Step 15: mean_absolute_error

--- model.py
+++ model.py
@@ -143,8 +143,10 @@ def ols_predict(X, theta):
 
     return pred
 
-# Step 15 - mean_absolute_error (not yet solved)
-# TODO: implement
+# Step 15 - mean_absolute_error
+def mean_absolute_error(y_true, y_pred):
+    
+    return np.mean(np.abs(y_true-y_pred))
 
 # Step 16 - root_mean_squared_error (not yet solved)
 # TODO: implement
