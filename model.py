a843729ff57b414eab32d26b643489ad816e1ab3
Mateusz Poszelężny <110680320+Mitfort@users.noreply.github.com>
2026-09-21 10:43:56 +0200

    Step 2: compute_iqr_bounds

--- model.py
+++ model.py
@@ -26,8 +26,18 @@ def impute_nan_with_mean(X):
 
     return X
 
-# Step 2 - compute_iqr_bounds (not yet solved)
-# TODO: implement
+# Step 2 - compute_iqr_bounds
+def compute_iqr_bounds(X, k=1.5):
+
+    q1 = np.percentile(X, 25, axis=0)
+    q3 = np.percentile(X, 75, axis=0)
+
+    iqr = q3 - q1
+
+    lower = q1 - k * iqr
+    upper = q3 + k * iqr
+
+    return (lower,upper)
 
 # Step 3 - clip_columns (not yet solved)
 # TODO: implement
